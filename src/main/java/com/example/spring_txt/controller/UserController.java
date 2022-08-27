698c30edfc185b4a64235b374cc379a23d10ad55
guluming <guluming@naver.com>
2022-08-27 22:34:40 +0900

    Feat: "추가 각각 조회 API 및 삭제 API 구현"
    - getUserList API
      ㄴ모든 User 를 json 형태로 응답
    - getUserId
      ㄴID 로 한명의 User 를 식별하여 json 형태로 응답
    - deleteUserId
      ㄴID 로 적재된 데이터를 삭제 후 정상 처리면 true 에러 발생 시 false 를 응답
    - getUserName
      ㄴ이름을 포함된 문자열 검색으로 찾아 json 형태로 응답

--- src/main/java/com/example/spring_txt/controller/UserController.java
+++ src/main/java/com/example/spring_txt/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.example.spring_txt.controller;
 
+import com.example.spring_txt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/api")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/user/list")
+    public ResponseEntity<Object> getUserList() {
+        return userService.getUserList();
+    }
+
+    @GetMapping("/user/id")
+    public ResponseEntity<Object> getUserId(@RequestParam("id") int id) {
+        return userService.getUserId(id);
+    }
+
+    @DeleteMapping("/user/id")
+    public ResponseEntity<Object> deleteUserId(@RequestParam("id") int id) {
+        return userService.deleteUserId(id);
+    }
+
+    @GetMapping("/user/name")
+    public ResponseEntity<Object> getUserName(@RequestParam("name") String name) {
+        return userService.getUserName(name);
+    }
 }
--- src/main/java/com/example/spring_txt/service/UserService.java
+++ src/main/java/com/example/spring_txt/service/UserService.java
@@ -1,4 +1,54 @@
 package com.example.spring_txt.service;
 
+import com.example.spring_txt.entity.User;
+import com.example.spring_txt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
 public class UserService {
+    private final UserRepository userRepository;
+
+    public ResponseEntity<Object> getUserList() {
+        List<User> userList = userRepository.findAll();
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> getUserId(int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            return new ResponseEntity<>("존재하지 않는 사용자 입니다.", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<Object> deleteUserId(int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        } else {
+            userRepository.delete(user.get());
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+    }
+
+    public ResponseEntity<Object> getUserName(String name) {
+        List<User> userList = userRepository.findByName(name);
+        if (userList.isEmpty()) {
+            return new ResponseEntity<>("해당되는 사용자가 없습니다.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(userList, HttpStatus.OK);
+        }
+    }
 }
